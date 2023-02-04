7a90db3243ecec02ad2e2f98069bd5f349892bdb
Mohammad anas khan <muhammedanas5186@gmail.com>
2023-02-03 20:59:17 -0800

    banner image made responsive

---
+++ src/Banner.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <body>
+        <div className='banner'>
+            <div className='slider-frame'>
+                <div className='slide-images'>
+                    <div className='img-container'>
+                        <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img>
+                    </div>
+                    <div className='img-container'>
+                        <img src=''></img>
+                    </div>
+                    <div className='img-container'>
+                        <img src=''></img>
+                    </div>
+                    <div className='img-container'>
+                        <img src=''></img>
+                    </div>
+                    <div className='img-container'>
+                        <img src=''></img>
+                    </div>
+                    <div className='img-container'>
+                        <img src=''></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    
+  )
+}
+
+export default Banner
---
+++ src/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from './Banner'
+import './HomeScreen.css'
+
+
+function HomeScreen() {
+  return (
+    <div className='homescreen'>
+        
+        <Banner/>
+        
+    </div>
+  )
+}
+
+export default HomeScreen
