dfdcc5f107e1b39bed8db8662cce8765a857551a
Atharva Bhawsar <atharvabhawsar18@gmail.com>
2023-02-04 12:29:01 +0530

    Banner gradient applied

--- src/Banner.js
+++ src/Banner.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import './Banner.css'
 
+
 function Banner() {
   return (
-    <body>
-        <div className='banner'>
+    <header
+      className='banner'
+      style={{
+        backgroundImage: <img src='./banner_images/1.jpg'></img>, 
+        backgroundPosition: `center center`
+      }}
+    >
+
             <div className='container'>
                 <div className='wrapper'>
                     
+                        {/* <img src='' className='img'></img> */}
+                        {/* <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img>
                         <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img>
                         <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img>
                         <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img>
-                        <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img>
-                        <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img>
-                        <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img>
-                </div>
+                        <img src='https://wallpaperaccess.com/full/11207.jpg' className='img'></img> */}
+                     
+                      </div> 
+                
             </div>
-        </div>
-    </body>
+
+      <div className="banner-fade-bottom" />
+    </header>
     
   )
 }
